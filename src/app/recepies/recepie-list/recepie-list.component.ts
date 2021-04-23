36536d8f86972a3810add84827bc1381e67c9901
DareDefyMe <70864245+DareDefyMe@users.noreply.github.com>
2021-04-23 09:25:33 +0500

    recepie template

--- src/app/recepies/recepie-list/recepie-list.component.ts
+++ src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recepie } from '../recepie.model';
 
 @Component({
   selector: 'app-recepie-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecepieListComponent implements OnInit {
 
+  recipes: Recepie[] = [
+    new Recepie(' Recipe', 'Best Turkey Sandwich', 'https://storcpdkenticomedia.blob.core.windows.net/media/recipemanagementsystem/media/recipe-media-files/recipes/foodservice/desktop%20images/2019_com_alfredo-dijon-turkey-sandwich_900x600.jpg?ext=.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recepies/recepies.component.ts
+++ src/app/recepies/recepies.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepies.component.css']
 })
 export class RecepiesComponent implements OnInit {
-  recipes = [];
+
 
   constructor() { }
 
